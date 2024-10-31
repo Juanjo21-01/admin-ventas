434d90a2633ee3ed2990360c37f28ec1a41bdb98
Juanjo21-01 <chevike15@gmail.com>
2024-10-30 22:58:34 -0600

    Actualiza las configuraciones CORS para permitir el acceso desde el dominio de producción

--- src/main/java/com/proyecto/ventas/config/WebConfig.java
+++ src/main/java/com/proyecto/ventas/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://intecap-ventas-grupo1.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
                 .allowCredentials(true);
